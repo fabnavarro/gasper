4790497530505b33fe74f1c01b8ca22e3997540e
fabnavarro <fabien.navarro@ensai.fr>
2020-07-27 16:36:30 +0200

    adapt adjency_mat argument to match swissroll

--- R/adjacency_mat.R
+++ R/adjacency_mat.R
@@ -6,21 +6,16 @@
 #' @param f is a scalar potential (\eqn{\exp(-x^2/2t^2)}{exp(-x^2/2*t^2)} for gaussian potential)
 #' @param s is a threhold to sparisfy the matrix
 #' @examples
-#' \dontrun{
-#' pts <- swissroll(N=500, seed=0, a=1, b=4)
+#' pts <- swissroll(N=100, seed=0, a=1, b=4)
 #' W <- adjacency_mat(pts)
-#' }
 #' @seealso \code{\link{laplacian_mat}}
-adjacency_mat <- function(pts, f = function(x) {
-  exp(-x^2/8)
-}, N = 500, s = 0) {
+adjacency_mat <- function(pts, f = function(x) {exp(-x^2/8)},
+                          s = 0) {
+  N <- nrow(pts)
   ptsi <- pts[rep(1:N, each = N), ]
   ptsj <- pts[rep(1:N, times = N), ]
   tmp <- f(sqrt(rowSums((ptsi - ptsj)^2)))
   res <- tmp * (abs(tmp) > s)
   dim(res) <- c(N, N)
-
-
-  # eps <- 2 * .Machine$double.eps res <- ifelse(abs(res) < eps, 0, res)
   return(res)
 }
