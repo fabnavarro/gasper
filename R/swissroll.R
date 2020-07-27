c80154a64d5c1fe9be5aee6154e13219185669d6
fabnavarro <fabien.navarro@ensai.fr>
2020-07-27 16:27:10 +0200

    option to not seed a seed in swissrool.R

--- R/swissroll.R
+++ R/swissroll.R
@@ -1,6 +1,6 @@
-#' Swiss Roll graph generation
+#' Swiss roll graph generation
 #'
-#' Map the square \eqn{[0,1]^2}{[0,1]^2} in swissroll
+#' Map the square \eqn{[0,1]^2}{[0,1]^2} in swiss roll
 #' for all \eqn{x,y} in \eqn{[0,1]^2}{[0,1]^2}, set
 #' \deqn{Sx=\pi \sqrt{(b^2-a^2)x + a^2)}}{Sx=pi*sqrt((b**2-a**2)*x+a**2)}
 #' \deqn{Sy=\pi (b^2-a^2)y/2}{Sy=pi**(b**2-a**2)*y/2}
@@ -9,13 +9,13 @@
 #' @import stats
 #' @param a,b Shape parameters.
 #' @param N Number of points drawn.
-#' @param seed To fix the seed (for reproducible experiments).
+#' @param seed Optionally specify a RNG seed (for reproducible experiments).
 #' @return N x 3 array for 3d points.
 #' @examples
 #' pts <- swissroll(N=500, seed=0, a=1, b=4)
 #' scatterplot3d::scatterplot3d(pts, y=NULL, z=NULL)
 #' @seealso \code{\link{adjacency_mat}}
-swissroll <- function(N = 500, seed = 0, a = 1, b = 4) {
+swissroll <- function(N = 500, seed = NULL, a = 1, b = 4) {
   set.seed(seed)
   x <- runif(N)
   y <- runif(N)
