349634013da6f556fe162a83cd2b8b554cfe0cc0
fabnavarro <fabien.navarro@ensai.fr>
2021-01-17 19:03:34 +0100

    add bool arg keepwc to SURE* funs

--- R/SURE_MSEthresh.R
+++ R/SURE_MSEthresh.R
@@ -16,51 +16,91 @@
 #' @param sigma Sd of the noise.
 #' @param hatsigma Estimator of the sd (if any).
 #' @param policy Dependent or uniform.
+#' @param keepwc Boolean allowing to export the coefficients of the frame after thresholding (TRUE by default).
 #' @return \code{res} a dataframe contening MSE, SURE, hatSURE and their respective minima
 #' @references
-#' de Loynes, B., Navarro, F., Olivier, B. (2019). Data-driven Thresholding in Denoising with Spectral Graph Wavelet Transform. arXiv preprint arXiv:1906.01882.
+#' de Loynes, B., Navarro, F., Olivier, B. (2021). Data-driven thresholding in denoising with Spectral Graph Wavelet Transform. Journal of Computational and Applied Mathematics, Vol. 389.
 
-SURE_MSEthresh <- function(wcn, wcf, tresh, diagWWt, b, sigma, hatsigma, policy) {
+SURE_MSEthresh <- function(wcn, wcf, tresh, diagWWt, b, sigma, hatsigma, policy, keepwc=TRUE) {
   nthresh <- length(tresh)
   erisk <- dof  <- MSE <- rep(0, nthresh)
-  wcs <- matrix(0, ncol=nthresh, nrow = length(wcn))
-  if (policy == "uniform") {
-    for (i in 1:nthresh) {
-      wc <- betathresh(wcn, tresh[i], b)
-      wcs[,i] <- wc
-      MSE[i] <- sum((wcf - wc)^2)
-      erisk[i] <- sum((wc - wcn)^2)
-      dof[i] <- 2*sum((abs(wcn) > tresh[i])*
-                      (1 + (b - 1)*tresh[i]^b/abs(wcn)^b)*diagWWt,na.rm=TRUE)
+  if(keepwc){
+    wcs <- matrix(0, ncol=nthresh, nrow = length(wcn))
+    if (policy == "uniform") {
+      for (i in 1:nthresh) {
+        wc <- betathresh(wcn, tresh[i], b)
+        wcs[,i] <- wc
+        MSE[i] <- sum((wcf - wc)^2)
+        erisk[i] <- sum((wc - wcn)^2)
+        dof[i] <- 2*sum((abs(wcn) > tresh[i])*
+                          (1 + (b - 1)*tresh[i]^b/abs(wcn)^b)*diagWWt,na.rm=TRUE)
+      }
     }
-  }
-  else if (policy == "dependent") {
-    for (i in 1:nthresh) {
-      wc <- betathresh(wcn, tresh[i]*sqrt(diagWWt), b)
-      wcs[,i] <- wc
-      MSE[i] <- sum((wcf - wc)^2)
-      erisk[i] <- sum((wc - wcn)^2)
-      dof[i] <- 2*sum((abs(wcn) > tresh[i]*sqrt(diagWWt))*
-                (1 + (b - 1)*(tresh[i]*sqrt(diagWWt))^b/abs(wcn)^b)*diagWWt,na.rm=TRUE)
+    else if (policy == "dependent") {
+      for (i in 1:nthresh) {
+        wc <- betathresh(wcn, tresh[i]*sqrt(diagWWt), b)
+        wcs[,i] <- wc
+        MSE[i] <- sum((wcf - wc)^2)
+        erisk[i] <- sum((wc - wcn)^2)
+        dof[i] <- 2*sum((abs(wcn) > tresh[i]*sqrt(diagWWt))*
+                          (1 + (b - 1)*(tresh[i]*sqrt(diagWWt))^b/abs(wcn)^b)*diagWWt,na.rm=TRUE)
+      }
+    } else {
+      warning("Allowable policy are listed above")
+      print("uniform")
+      print("dependent")
     }
-  } else {
-    warning("Allowable policy are listed above")
-    print("uniform")
-    print("dependent")
+    #MSE <- colSums((as.vector(wcf) - wcs)^2)
+    #erisk <- colSums((wcs - as.vector(wcn))^2)
+    SURE <- erisk + dof*sigma^2
+    hatSURE <- erisk + dof*hatsigma^2
+    minMSE <- which.min(MSE)
+    minSURE <- which.min(SURE)
+    minhatSURE <- which.min(hatSURE)
+    opthreshMSE <- tresh[minMSE]
+    opthreshSURE <- tresh[minSURE]
+    opthreshhatSURE <- tresh[minhatSURE]
+    res <- list("wc"=wcs,
+                "res"=data.frame(MSE = MSE, SURE = SURE, hatSURE = hatSURE),
+                "min"=c(minMSE, minSURE, minhatSURE),
+                "thr"=c(opthreshMSE, opthreshSURE, opthreshhatSURE))
   }
-  #MSE <- colSums((as.vector(wcf) - wcs)^2)
-  #erisk <- colSums((wcs - as.vector(wcn))^2)
-  SURE <- erisk + dof*sigma^2
-  hatSURE <- erisk + dof*hatsigma^2
-  minMSE <- which.min(MSE)
-  minSURE <- which.min(SURE)
-  minhatSURE <- which.min(hatSURE)
-  opthreshMSE <- tresh[minMSE]
-  opthreshSURE <- tresh[minSURE]
-  opthreshhatSURE <- tresh[minhatSURE]
-  res <- list("wc"=wcs,
-              "res"=data.frame(MSE = MSE, SURE = SURE, hatSURE = hatSURE),
-              "min"=c(minMSE, minSURE, minhatSURE),
-              "thr"=c(opthreshMSE, opthreshSURE, opthreshhatSURE))
+  if(!keepwc){
+    if (policy == "uniform") {
+      for (i in 1:nthresh) {
+        wc <- betathresh(wcn, tresh[i], b)
+        MSE[i] <- sum((wcf - wc)^2)
+        erisk[i] <- sum((wc - wcn)^2)
+        dof[i] <- 2*sum((abs(wcn) > tresh[i])*
+                          (1 + (b - 1)*tresh[i]^b/abs(wcn)^b)*diagWWt,na.rm=TRUE)
+      }
+    }
+    else if (policy == "dependent") {
+      for (i in 1:nthresh) {
+        wc <- betathresh(wcn, tresh[i]*sqrt(diagWWt), b)
+        MSE[i] <- sum((wcf - wc)^2)
+        erisk[i] <- sum((wc - wcn)^2)
+        dof[i] <- 2*sum((abs(wcn) > tresh[i]*sqrt(diagWWt))*
+                          (1 + (b - 1)*(tresh[i]*sqrt(diagWWt))^b/abs(wcn)^b)*diagWWt,na.rm=TRUE)
+      }
+    } else {
+      warning("Allowable policy are listed above")
+      print("uniform")
+      print("dependent")
+    }
+    SURE <- erisk + dof*sigma^2
+    hatSURE <- erisk + dof*hatsigma^2
+    minMSE <- which.min(MSE)
+    minSURE <- which.min(SURE)
+    minhatSURE <- which.min(hatSURE)
+    opthreshMSE <- tresh[minMSE]
+    opthreshSURE <- tresh[minSURE]
+    opthreshhatSURE <- tresh[minhatSURE]
+    res <- list("res"=data.frame(MSE = MSE,
+                                 SURE = SURE,
+                                 hatSURE = hatSURE),
+                "min"=c(minMSE, minSURE, minhatSURE),
+                "thr"=c(opthreshMSE, opthreshSURE, opthreshhatSURE))
+  }
   return(res)
 }
--- R/SUREthresh.R
+++ R/SUREthresh.R
@@ -12,47 +12,81 @@
 #' @param sigma Sd of the noise.
 #' @param hatsigma Estimator of the sd (if any).
 #' @param policy Dependent or uniform.
+#' @param keepwc Boolean allowing to export the coefficients of the frame after thresholding (TRUE by default).
 #' @return \code{res} a dataframe contening SURE, hatSURE and their respective minima.
 #' @references
-#' de Loynes, B., Navarro, F., Olivier, B. (2019). Data-driven Thresholding in Denoising with Spectral Graph Wavelet Transform. arXiv preprint arXiv:1906.01882.
+#' de Loynes, B., Navarro, F., Olivier, B. (2021). Data-driven thresholding in denoising with Spectral Graph Wavelet Transform. Journal of Computational and Applied Mathematics, Vol. 389.
 
-SUREthresh <- function(wcn, tresh, diagWWt, b, sigma, hatsigma, policy) {
+SUREthresh <- function(wcn, tresh, diagWWt, b, sigma, hatsigma, policy, keepwc=TRUE) {
   nthresh <- length(tresh)
   erisk <- dof  <- MSE <- rep(0, nthresh)
-  wcs <- matrix(0, ncol=nthresh, nrow = length(wcn))
-  if (policy == "uniform") {
-    for (i in 1:nthresh) {
-      wc <- betathresh(wcn, tresh[i], b)
-      wcs[,i] <- wc
-      erisk[i] <- sum((wc - wcn)^2)
-      dof[i] <- 2*sum((abs(wcn) > tresh[i])*
-                        (1 + (b - 1)*tresh[i]^b/abs(wcn)^b)*diagWWt,na.rm=TRUE)
+  if(keepwc){
+    wcs <- matrix(0, ncol=nthresh, nrow = length(wcn))
+    if (policy == "uniform") {
+      for (i in 1:nthresh) {
+        wc <- betathresh(wcn, tresh[i], b)
+        wcs[,i] <- wc
+        erisk[i] <- sum((wc - wcn)^2)
+        dof[i] <- 2*sum((abs(wcn) > tresh[i])*
+                          (1 + (b - 1)*tresh[i]^b/abs(wcn)^b)*diagWWt,na.rm=TRUE)
+      }
     }
-  }
-  else if (policy == "dependent") {
-    for (i in 1:nthresh) {
-      wc <- betathresh(wcn, tresh[i]*sqrt(diagWWt), b)
-      wcs[,i] <- wc
-      erisk[i] <- sum((wc - wcn)^2)
-      dof[i] <- 2*sum((abs(wcn) > tresh[i]*sqrt(diagWWt))*
-                        (1 + (b - 1)*(tresh[i]*sqrt(diagWWt))^b/abs(wcn)^b)*diagWWt,na.rm=TRUE)
+    else if (policy == "dependent") {
+      for (i in 1:nthresh) {
+        wc <- betathresh(wcn, tresh[i]*sqrt(diagWWt), b)
+        wcs[,i] <- wc
+        erisk[i] <- sum((wc - wcn)^2)
+        dof[i] <- 2*sum((abs(wcn) > tresh[i]*sqrt(diagWWt))*
+                          (1 + (b - 1)*(tresh[i]*sqrt(diagWWt))^b/abs(wcn)^b)*diagWWt,na.rm=TRUE)
+      }
+    } else {
+      warning("Allowable policy are listed above")
+      print("uniform")
+      print("dependent")
     }
-  } else {
-    warning("Allowable policy are listed above")
-    print("uniform")
-    print("dependent")
+    #MSE <- colSums((as.vector(wcf) - wcs)^2)
+    #erisk <- colSums((wcs - as.vector(wcn))^2)
+    SURE <- erisk + dof*sigma^2
+    hatSURE <- erisk + dof*hatsigma^2
+    minSURE <- which.min(SURE)
+    minhatSURE <- which.min(hatSURE)
+    opthreshSURE <- tresh[minSURE]
+    opthreshhatSURE <- tresh[minhatSURE]
+    res <- list("wc"=wcs,
+                "res"=data.frame(SURE = SURE, hatSURE = hatSURE),
+                "min"=c(minSURE, minhatSURE),
+                "thr"=c(opthreshSURE, opthreshhatSURE))
   }
-  #MSE <- colSums((as.vector(wcf) - wcs)^2)
-  #erisk <- colSums((wcs - as.vector(wcn))^2)
-  SURE <- erisk + dof*sigma^2
-  hatSURE <- erisk + dof*hatsigma^2
-  minSURE <- which.min(SURE)
-  minhatSURE <- which.min(hatSURE)
-  opthreshSURE <- tresh[minSURE]
-  opthreshhatSURE <- tresh[minhatSURE]
-  res <- list("wc"=wcs,
-              "res"=data.frame(SURE = SURE, hatSURE = hatSURE),
-              "min"=c(minSURE, minhatSURE),
-              "thr"=c(opthreshSURE, opthreshhatSURE))
+  if(!keepwc){
+    if (policy == "uniform") {
+      for (i in 1:nthresh) {
+        wc <- betathresh(wcn, tresh[i], b)
+        erisk[i] <- sum((wc - wcn)^2)
+        dof[i] <- 2*sum((abs(wcn) > tresh[i])*
+                          (1 + (b - 1)*tresh[i]^b/abs(wcn)^b)*diagWWt,na.rm=TRUE)
+      }
+    }
+    else if (policy == "dependent") {
+      for (i in 1:nthresh) {
+        wc <- betathresh(wcn, tresh[i]*sqrt(diagWWt), b)
+        erisk[i] <- sum((wc - wcn)^2)
+        dof[i] <- 2*sum((abs(wcn) > tresh[i]*sqrt(diagWWt))*
+                          (1 + (b - 1)*(tresh[i]*sqrt(diagWWt))^b/abs(wcn)^b)*diagWWt,na.rm=TRUE)
+      }
+    } else {
+      warning("Allowable policy are listed above")
+      print("uniform")
+      print("dependent")
+    }
+    SURE <- erisk + dof*sigma^2
+    hatSURE <- erisk + dof*hatsigma^2
+    minSURE <- which.min(SURE)
+    minhatSURE <- which.min(hatSURE)
+    opthreshSURE <- tresh[minSURE]
+    opthreshhatSURE <- tresh[minhatSURE]
+    res <- list("res"=data.frame(SURE = SURE, hatSURE = hatSURE),
+                "min"=c(minSURE, minhatSURE),
+                "thr"=c(opthreshSURE, opthreshhatSURE))
+  }
   return(res)
 }
