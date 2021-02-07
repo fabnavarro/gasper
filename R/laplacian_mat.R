1bd4a76a9ef6e39f5d1350a6f89feb2b3226d7d5
fabnavarro <fabien.navarro@ensai.fr>
2021-02-08 00:02:47 +0100

    adapt Laplacian_mat to sparse matrix

--- R/laplacian_mat.R
+++ R/laplacian_mat.R
@@ -3,9 +3,15 @@
 #' Compute the (unormalized) laplacian matrix from the adjacency matrix.
 #'
 #' @export laplacian_mat
+#' @importFrom methods is
+#' @importFrom Matrix Diagonal
 #' @param W Adjacency matrix.
 #'
 laplacian_mat <- function(W) {
-  D <- diag(rowSums(W))
+  if(is(W, 'sparseMatrix')){
+    D <- Diagonal(nrow(W), rowSums(W))
+  } else {
+    D <- rowSums(W)
+  }
   return(D - W)
 }
