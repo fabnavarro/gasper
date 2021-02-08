c7a94e5f609826fccf6a44910f73535b1784f69e
fabnavarro <fabien.navarro@ensai.fr>
2021-02-08 20:04:19 +0100

    adapt eigensort to sparse matrix

--- R/eigensort.R
+++ R/eigensort.R
@@ -4,7 +4,7 @@
 #' (output sorted in increasing order).
 #'
 #' @export eigensort
-#' @param x Symetric matrix whose spectral decomposition is to be computed.
+#' @param x Symetric matrix (sparse or dense) whose spectral decomposition is to be computed.
 #' @examples
 #' A <- matrix(1, ncol=2, nrow=2)
 #' dec <- eigensort(A)
@@ -12,5 +12,6 @@
 eigensort <- function(x) {
   dec <- eigen(x, symmetric = T)
   A <- dec$vectors
-  return(list(evalues = rev(dec$values), evectors = A[, ncol(A):1]))
+  return(list(evalues = rev(dec$values),
+              evectors = A[, ncol(A):1]))
 }
