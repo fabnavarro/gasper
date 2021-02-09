33a52cff9b13752d89ea04c355df61889e31c118
fabnavarro <fabien.navarro@ensai.fr>
2021-02-09 14:29:08 +0100

    add new plot_filter function

---
+++ R/plot_filter.R
@@ -0,0 +1,18 @@
+#' Plot tight-frame filters.
+#'
+#' Plot tight-frame kernels/filters.
+#'
+#' @export plot_filter
+#' @param lmax Largest eigenvalues of the Laplacian matrix.
+#' @param b Parameter that control the number of scales.
+
+plot_filter <- function(lmax, b, N=1000){
+  kmax <- floor(log(lmax)/log(b)) + 2
+  x <- seq(0, lmax, length.out = 1000)
+  plot(x, sqrt(zetav(x, 0, b)), type="l",
+       xlab = expression(lambda), ylab = "", lwd=2)
+  for (j in 1:kmax){
+    lines(x, sqrt(zetav(x, j, b)), col=j+1,
+          lwd=2)
+  }
+}
