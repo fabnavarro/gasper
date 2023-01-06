88242d86d731c29b7e9a0dbd182c047c76b27006
fabnavarro <fabien.navarro@ensai.fr>
2023-01-06 19:12:32 +0100

    store grid1 graph

---
+++ R/grid1.R
@@ -0,0 +1,10 @@
+#' grid1 graph.
+#'
+#' A dataset containing grid1 graph.
+#'
+#' @format list of 2 elements
+#' \describe{
+#' \item{xy}{coordinates}
+#' \item{sA}{adjacency matrix}
+#' }
+"grid1"
