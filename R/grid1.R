5788c5f2c1f15d0c9219238ad7761adccecb8451
fabnavarro <fabien.navarro@ensai.fr>
2023-08-26 15:36:34 +0200

    remove grid1 download

--- R/grid1.R
+++ R/grid1.R
@@ -2,9 +2,11 @@
 #'
 #' A dataset containing grid1 graph.
 #'
-#' @format list of 2 elements
+#' @format list of 4 elements
 #' \describe{
 #' \item{xy}{coordinates}
 #' \item{sA}{adjacency matrix}
+#' \item{dim}{numbers of rows, columns and numerically nonzero elements}
+#' \item{info}{information about the matrix}
 #' }
 "grid1"
