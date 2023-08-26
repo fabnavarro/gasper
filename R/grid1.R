c6a734b26fda22dc38119317976724c856ad3e08
fabnavarro <fabien.navarro@ensai.fr>
2023-08-26 16:52:34 +0200

    try to match CRAN policy to fail gracefully

--- R/grid1.R
+++ R/grid1.R
@@ -2,11 +2,10 @@
 #'
 #' A dataset containing grid1 graph.
 #'
-#' @format list of 4 elements
+#' @format list of 3 elements
 #' \describe{
 #' \item{xy}{coordinates}
 #' \item{sA}{adjacency matrix}
 #' \item{dim}{numbers of rows, columns and numerically nonzero elements}
-#' \item{info}{information about the matrix}
 #' }
 "grid1"
