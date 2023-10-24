7edadad6aa8be6bdd1bd580a1cf5a57459e9390a
fabnavarro <fabien.navarro@ensai.fr>
2023-10-24 23:26:59 +0200

    update HPFVN doc

--- R/HPFVN.R
+++ R/HPFVN.R
@@ -1,12 +1,13 @@
 #' High Pass Filter Von Neumann Estimator.
 #'
-#' Graph extension of the Von Neummann variance estimator using finest scale coefficients.
+#' \code{HPFVN} computes graph extension of the Von Neummann variance estimator using finest scale coefficients (as in classical wavelet approaches).
 #'
 #' @export HPFVN
-#' @param wcn Noisy wavelet coefficients.
-#' @param b Parameter that control the number of scales.
-#' @param evalues Laplacian spectrum.
+#' @param wcn Noisy wavelet coefficients (numeric vector).
+#' @param b Parameter that control the number of scales (numeric).
+#' @param evalues Laplacian spectrum (numeric vector).
 #' @examples
+#' \dontrun{
 #' data(grid1)
 #' A <- grid1$sA
 #' L <- laplacian_mat(A)
@@ -22,14 +23,18 @@
 #' b <- 2
 #' wcn <- forward_sgwt(y, evalues, evectors, b=b)
 #' sigma^2
-#' HPFVN(wcn, evalues, b)
+#' HPFVN(wcn, evalues, b)}
 #' @seealso \code{\link{GVN}}
 #' @references
-#' von Neumann, J. (1941). Distribution of the ratio of the mean square successive difference to the variance. \emph{Ann. Math. Statistics}, 35(3), 433--451.
+#'
+#' Donoho, D. L., & Johnstone, I. M. (1994). Ideal spatial adaptation by wavelet shrinkage. biometrika, 81(3), 425-455.
 #'
 #' de Loynes, B., Navarro, F., Olivier, B. (2021). Data-driven thresholding in denoising with Spectral Graph Wavelet Transform. Journal of Computational and Applied Mathematics, Vol. 389.
-#' @details
 #'
+#' von Neumann, J. (1941). Distribution of the ratio of the mean square successive difference to the variance. \emph{Ann. Math. Statistics}, 35(3), 433--451.
+#'
+#' @details
+#' The High Pass Filter Von Neumann Estimator (HPFVN) is the graph analog of the classical Von Neumann estimator, focusing on the finest scale coefficients. It leverages the characteristics of the graph signal's wavelet coefficients to estimate the variance:
 #' \deqn{\hat \sigma^2 = \frac{\sum_{i=nJ+1}^{n(J+1)} (\mathcal{W} y)^2_i}{\mathrm{Tr}~\psi_J(L)}}
 
 HPFVN <- function(wcn, evalues, b){
