5c21aafffd61fcfa1cf82e76479c0c9c19d95747
fabnavarro <fabien.navarro@ensai.fr>
2023-10-24 18:44:51 +0200

    add details SNR/PSNR fun

--- R/PSNR.R
+++ R/PSNR.R
@@ -1,12 +1,19 @@
-#' Peak Signal to Noise Ratio.
+#' Compute the Peak Signal to Noise Ratio (PSNR).
 #'
-#' Compute the Peack Signal to Noise Ratio, defined by:
-#' \deqn{PSNR(x,y)=10 \log_{10}( \max(\max(x),\max(y))^2 / |x-y|^2 )}{PSNR(x,y)=10*log10( max(max(x),max(y))^2 / |x-y|^2 )}
+#' \code{PSNR} function computes the PSNR between two signals or images.
 #'
 #' @export PSNR
-#' @param x Original reference signal/image.
-#' @param y Restored or noisy signal/image.
-#' @return Peak Signal to Noise ratio.
+#' @param x Original reference signal/image (numeric vector/matrix).
+#' @param y Restored or noisy signal/image (numeric vector/matrix).
+#' @return \code{PSNR} Peak Signal to Noise Ratio (numeric).
+#'
+#' @details
+#'
+#'Higher values of PSNR indicate closer similarity between the original and the compared signal or image.
+#'
+#' The PSNR is defined by:
+#' \deqn{PSNR(x,y) = 10 \log_{10}\left(\frac{\max(\max(x),\max(y))^2}{\mathrm{MSE}(x, y)}\right)}{PSNR(x,y) = 10*log10( max(max(x),max(y))^2 / mean((x-y)^2) )}
+#'
 #' @examples
 #' x <- cos(seq(0, 10, length=100))
 #' y <- x + rnorm(100, sd=0.5)
@@ -14,8 +21,10 @@
 #' @seealso \code{\link{SNR}}
 
 PSNR <- function(x, y){
-  d <- mean((x-y)^2)
+  d <- mean((x - y)^2)
   m <- max(abs(x))
-  p <- 10*log10(m^2/d)
+  p <- 10 * log10(m^2 / d)
   return(p)
 }
+
+
--- R/SNR.R
+++ R/SNR.R
@@ -1,9 +1,20 @@
-#' Signal to Noise Ratio.
+#' Compute the Signal to Noise Ratio (SNR).
+#'
+#' \code{SNR} computes the SNR between two signals, indicating the level of desired signal to the level of background noise.
 #'
 #' @export SNR
-#' @param x Original reference signal.
-#' @param y Restored or noisy signal.
-#' @return Signal to Noise ratio.
+#' @param x Original reference signal (numeric vector/matrix).
+#' @param y Restored or noisy signal (numeric vector/matrix).
+#' @return \code{SNR} Signal to Noise Ratio (numeric).
+#'
+#' @details
+#'
+#' Higher values of SNR indicate a cleaner signal compared to the noise level. The SNR is computed as the ratio of the power of the signal (or the square of the Euclidean norm of the signal) to the power of the noise (or the square of the Euclidean norm of the signal difference), represented in decibels (dB).
+#'
+#' The SNR is defined by:
+#' \deqn{SNR(x,y) = 20 \log_{10}\left(\frac{\|x\|_2}{\|x-y\|_2}\right)}{SNR(x,y) = 20*log10( norm(x, "2") / norm(x-y, "2") )}
+#'
+#'
 #' @examples
 #' x <- cos(seq(0, 10, length=100))
 #' y <- x + rnorm(100, sd=0.5)
