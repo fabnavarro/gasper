4d4518017a0e8187b6888b99217cb1317449cc69
fabnavarro <fabien.navarro@ensai.fr>
2023-11-03 10:43:27 +0100

    doc `SuiteSparseData`

--- R/SuiteSparseData.R
+++ R/SuiteSparseData.R
@@ -1,29 +1,26 @@
 #' Matrix Data from SuiteSparse Matrix Collection
 #'
-#' This dataset represents a collection of matrices from the SuiteSparse Matrix Collection
-#' (formerly known as the University of Florida Sparse Matrix Collection). The dataset
-#' includes matrices from various domains such as structural engineering, computational
-#' fluid dynamics, optimization, economic modeling, and more. The structure of this
-#' dataframe mirrors the structure presented on the SuiteSparse Matrix Collection website.
-#'
-#' The SuiteSparse Matrix Collection is a large set of sparse matrices
-#' that arise in real applications. It is widely used by the numerical linear algebra
-#' community for the development and performance evaluation of sparse matrix algorithms.
-#' The Collection covers a wide spectrum of domains, including both geometric and
-#' non-geometric domains.
+#' This dataset represents the collection of matrices from the SuiteSparse Matrix Collection. The structure of the dataframe mirrors the structure presented on the SuiteSparse Matrix Collection website.
 #'
 #' @format A data frame with 2893 rows and 8 columns:
 #' \describe{
-#'   \item{\code{ID}}{Integer, Unique identifier for each matrix.}
-#'   \item{\code{Name}}{Character, Name of the matrix.}
-#'   \item{\code{Group}}{Character, Group name the matrix belongs to.}
-#'   \item{\code{Rows}}{Integer, Number of rows in the matrix.}
-#'   \item{\code{Cols}}{Integer, Number of columns in the matrix.}
-#'   \item{\code{Nonzeros}}{Integer, Number of non-zero elements in the matrix.}
-#'   \item{\code{Kind}}{Character, Kind or category of the matrix.}
-#'   \item{\code{Date}}{Character, Date when the matrix was added or updated.}
+#'   \item{\code{ID}}{Integer. Unique identifier for each matrix.}
+#'   \item{\code{Name}}{Character. Name of the matrix.}
+#'   \item{\code{Group}}{Character. Group name the matrix belongs to.}
+#'   \item{\code{Rows}}{Integer. Number of rows in the matrix.}
+#'   \item{\code{Cols}}{Integer. Number of columns in the matrix.}
+#'   \item{\code{Nonzeros}}{Integer. Number of non-zero elements in the matrix.}
+#'   \item{\code{Kind}}{Character. Kind or category of the matrix.}
+#'   \item{\code{Date}}{Character. Date when the matrix was added or updated.}
 #' }
 #' @source SuiteSparse Matrix Collection website: \url{https://sparse.tamu.edu/}
+#'
+#' @note Data download date: 2023-11-01
+#' Note that the number of matrices in the SuiteSparse Matrix Collection may have increased since this download date.
+#'
+#' @details
+#' The SuiteSparse Matrix Collection is a large set of sparse matrices that arise in real applications. It is widely used by the numerical linear algebra community for the development and performance evaluation of sparse matrix algorithms. The Collection covers a wide spectrum of domains, including both geometric and non-geometric domains.
+#'
 #' @references
 #' Davis, T. A., & Hu, Y. (2011). The University of Florida sparse matrix collection. ACM Transactions on Mathematical Software (TOMS), 38(1), 1-25.
 #'
